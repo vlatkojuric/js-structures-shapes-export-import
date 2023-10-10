8cee1d71d8231f8893bdab852f23a25138bc0f4f
Vlatko Juric <juricv78gmail.com>
2023-10-10 09:10:19 +0200

    first js modules exericse

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 console.clear();
-
+import getRandomColor from "./utils/randomColor.js";
 const root = document.getElementById("root");
 
 const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
+  circle.style.backgroundColor = getRandomColor();
 });
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  square.style.backgroundColor = getRandomColor();
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  pentagon.style.backgroundColor = getRandomColor();
 });
 
 root.append(circle, square, pentagon);
